074e560178a7ce7d9446d246960bb59778b8420d
[GAURAV BHOITE] <[gauravbhoite1919@gmail.com]>
2022-07-03 00:15:08 +0530

    HII RUSHI

--- src/Action/Action_class1.java
+++ src/Action/Action_class1.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class Action_class1 {
+public class Action_class1 {//HII RUSHI
 
 	public static void main(String[] args) throws InterruptedException 
 	{
